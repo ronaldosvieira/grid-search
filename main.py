1f8b37874b383af6ae1bef22b8ea9f007d018997
Ronaldo Vieira <ronaldoesv@gmail.com>
2018-05-05 20:03:26 +0000

    Refactors pre-processing workflow

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 import sys
 from search import *
 
-raw_data = sys.argv
+def create_instance(width, height, grid, goal):
+    instance = Instance()
+    
+    for i in range(0, height):
+        for j in range(0, width):
+            instance.add_state(i, j, grid[i][j] == '.')
+            
+    for i in range(0, height):
+        for j in range(0, width):
+            for x in range(-1 if i > 0 else 0, 2 if i < height - 1 else 0):
+                for y in range(-1 if j > 0 else 0, 2 if j < width - 1 else 0):
+                    instance.add_successor(i, j, i + x, j + y)
+                    
+    instance.set_goal(instance.states[goal])
+    
+    return instance
+
+def solve(instance, algorithm, heuristic, start):
+    if algorithm == 'a-star':
+        if heuristic is None:
+            usage_error("heuristic is mandatory for a-star")
+            
+        if heuristic not in ['manhattan', 'octile']:
+            usage_error("invalid heuristic")
+            
+        return None
+            
+    elif algorithm == 'best-fit':
+        return None
+    elif algorithm == 'uniform-cost':
+        return None
+    elif algorithm == 'iterative-deepening':
+        return None
+    else:
+        usage_error("invalid algorithm")
 
 def usage_error(error = None):
     if error is not None:
@@ -10,11 +44,18 @@ def usage_error(error = None):
     print("usage: %s map x_s y_s x_g y_g algorithm heuristic?" % raw_data[0])
     sys.exit(1)
 
+raw_data = sys.argv
+
 if len(raw_data) not in [7, 8]:
     usage_error()
 
 map_name, algorithm = raw_data[1], raw_data[6]
 x_s, y_s, x_g, y_g = map(int, raw_data[2:6])
+
+try:
+    heuristic = raw_data[7]
+except:
+    heuristic = None
 
 with open("maps/" + map_name, 'r') as file:
     data = file.readlines()
@@ -23,36 +64,6 @@ height = int(data[1].split()[1])
 width = int(data[2].split()[1])
 grid = list(map(lambda l: list(l.rstrip()), data[4:]))
 
-instance = Instance()
+instance = create_instance(width, height, grid, (x_g, y_g))
 
-for i in range(0, height):
-    for j in range(0, width):
-        instance.add_state(i, j, grid[i][j] == '.')
-        
-for i in range(0, height):
-    for j in range(0, width):
-        for x in range(-1 if i > 0 else 0, 2 if i < height - 1 else 0):
-            for y in range(-1 if j > 0 else 0, 2 if j < width - 1 else 0):
-                instance.add_successor(i, j, i + x, j + y)
-                
-instance.set_goal(instance.states[(x_g, y_g)])
-
-if algorithm == 'a-star':
-    try:
-        heuristic = raw_data[7]
-    except:
-        usage_error("heuristic is mandatory for a-star")
-        
-    if heuristic not in ['manhattan', 'octile']:
-        usage_error("invalid heuristic")
-        
-    pass
-        
-elif algorithm == 'best-fit':
-    pass
-elif algorithm == 'uniform-cost':
-    pass
-elif algorithm == 'iterative-deepening':
-    pass
-else:
-    usage_error("invalid algorithm")
+solution = solve(instance, algorithm, heuristic, (x_s, y_s))
